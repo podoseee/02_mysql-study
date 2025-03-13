39edbc8472c8c52ab54ecbcef0583fee0fd9f22f
sotogito <sotogitoarchive@gmail.com>
2025-03-13 11:01:20 +0900

    feat : 3 GROUP BY HAVING 실습문제 풀이

--- sooyeon-mysql-workspace/practice/03_GROUP_BY_HAVING_실습문제_empdb.sql
+++ sooyeon-mysql-workspace/practice/03_GROUP_BY_HAVING_실습문제_empdb.sql
@@ -72,17 +72,30 @@ SELECT
         WHEN SUBSTRING(EMP_NO, 8, 1) IN ('2', '4') THEN '여자'
         ELSE '논바이너리'
     END AS 성별,
-    AVG(SALARY),
-    SUM(SALARY),
+    FORMAT(AVG(SALARY),0),
+    FORMAT(SUM(SALARY),0),
     COUNT(*)
 FROM
     employee
 GROUP BY
+    성별
+;
+
+SELECT
     CASE
         WHEN SUBSTRING(EMP_NO, 8, 1) IN ('1', '3') THEN '남자'
-        WHEN SUBSTRING(EMP_NO, 8, 1) IN ('2', '4') THEN '여자'
-        ELSE '논바이너리'
-    END;
+        ELSE '여자'
+    END AS 성별,
+    FORMAT(AVG(SALARY),0),
+    FORMAT(SUM(SALARY),0),
+    COUNT(*)
+FROM
+    employee
+GROUP BY
+    성별
+;
+# 내가 조건을 ㄷ잘못 달아줌 그냥 1을 넣는 것은 if(1) 이라고 하는것ㄱ돠 똑같기ㅜ때문에 항상 참임 -  WHEN 1 THEN '남자'
+
     # SUBSTRING(EMP_NO,8,1)만 사용할 경우에는 1,2,3,4 총 4경우가 있어서 하나를 지정해주고 하는게 맞다.
     # 그니가 내가 코드가 틀린 이유는 SUBSTRING(EMP_NO,8,1) IN (1,3)이걸로 그룹바이로 하면 if(남자) else(여자) 로 처리가 되고 SELECT에서 CASE WHEN IN THEN으로 판단할 때 값이 0과 1이 오느느데 그걸로 CASE 문법을 판단할 ㅅ ㅜ없다는 거지 원래는 보통 EMP_NO로하면 값이 전달되어서 판단할 수 있는데 내가 조건을 잘못단거야?
     # 즉 그룹핑했을때 어떤 조건으로 분별하고 어떤 값이 기준이 되느냐가 중요하다
@@ -98,3 +111,19 @@ GROUP BY
     J6              6
     J7              4
 */
+SELECT
+    JOB_CODE
+    ,COUNT(JOB_CODE)
+FROM
+    employee
+GROUP BY
+    JOB_CODE
+HAVING
+    COUNT(JOB_CODE) >= 3
+;
+
+
+
+
+
+
