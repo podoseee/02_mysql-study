f5b8554a3ab8897dbd2087a66aecee7d39f21a32
Kyungbae_Go <gokyungbae@gmail.com>
2025-03-16 10:25:52 +0900

    03.14 chundb 실습문제 4 완료

--- kyungbae-mysql-workspace/03.14_chundb실습문제4_DDL,DML_수업용계정.sql
+++ kyungbae-mysql-workspace/03.14_chundb실습문제4_DDL,DML_수업용계정.sql
@@ -116,7 +116,10 @@ COMMIT;
 -- 10. 춘 기술대학교 학생들의 정보가 포함되어 있는 학생일반정보테이블을 생성하고자 한다. 아래의 요구사항을 참고하여 작성하시오.
 CREATE TABLE tb_학생일반정보
 AS
-SELECT  student_no, student_name, student_address
+SELECT  
+    student_no 학번
+    , student_name 학생이름
+    , student_address 주소
 FROM tb_student;
 SELECT * FROM tb_학생일반정보;
 /*
@@ -134,8 +137,21 @@ SELECT * FROM tb_학생일반정보;
 
 
 
-      
+SELECT * FROM tb_student;
+SELECT * FROM tb_professor;
 -- 11. 국어국문학과 학생들의 정보만이 포함되어 있는 학과정보 테이블을 만들고자 한다. 아래의 요구사항을 참고하여 작성하시오.
+CREATE TABLE tb_국어국문학과
+AS
+SELECT
+    student_no AS 학번
+    , student_name AS 학생이름
+    , DATE_FORMAT(LEFT(student_ssn, 6), '%Y') AS 출생년도
+    , professor_name AS 교수이름
+FROM
+    tb_student
+    JOIN tb_professor ON professor_no = coach_professor_no;
+SELECT * FROM tb_국어국문학과;
+
 /*
     *  요구사항
     1) 테이블명 : tb_국어국문학과
@@ -156,31 +172,79 @@ SELECT * FROM tb_학생일반정보;
     
 -- 12. 현 학과들의 정원을 10% 증가시키게 되었다. 이에 사용할 SQL문을 작성하시오. 
 --     단, 반올림을 사용하여 소수점 자릿수는 생기지 않도록 한다. 
-
-
+SELECT * FROM tb_department;
+UPDATE tb_department
+SET capacity = ROUND(capacity * 1.1);
 
    
 -- 13. 학번 A413042 인 박건우 학생의 주소가 "서울시 종로구 숭인동 181-21"로 변경되었다고 한다.
 --     주소지를 정정하기 위해 사용할 SQL문을 작성하시오.
-
+SELECT * FROM tb_student WHERE student_no = 'A413042';
+UPDATE tb_student
+SET student_address = '서울시 종로구 숭인동 181-21'
+WHERE student_no = 'A413042';
 
 
 
 -- 14. 주민등록번호 보호법에 따라 학생정보 테이블에서 주민번호 뒷자리를 저장하지 않기로 결정하였다. 
 --     이 내용을 반영할 적절한 SQL문을 작성하시오.
-
+UPDATE tb_student
+SET student_ssn = LEFT(student_ssn, 8);
 
 
    
 -- 15. 의학과 김명훈 학생은 2005년 1학기에 자신이 수강한 "피부생리학" 점수가 잘못되었다는 것을 발견하고는 정정요청하였다.
 --     담당 교수의 확인 결과 해당 과목의 학점을 3.5로 변경하기로 결정되었다. 이 내용을 반영할 적절한 SQL문을 작성하시오.
-
-
-
-                
+SELECT * FROM tb_student;
+SELECT * FROM tb_grade;
+SELECT * FROM tb_class;
+SELECT * FROM tb_department;
+UPDATE
+    tb_grade
+SET
+    point = 3.5
+WHERE
+    (student_no, class_no, term_no) = (/*의학과 김명훈*/
+                                        SELECT *
+                                        FROM(
+                                            SELECT
+                                                g.student_no
+                                                , g.class_no
+                                                , g.term_no
+                                            FROM
+                                                tb_student s
+                                                JOIN tb_department d ON d.department_no = s.department_no
+                                                JOIN tb_grade g ON g.student_no = s.student_no
+                                                JOIN tb_class c ON c.class_no = g.class_no
+                                            WHERE
+                                                student_name = '김명훈'
+                                            AND department_name = '의학과'
+                                            AND class_name = '피부생리학'
+                                            AND term_no = '200501'
+                                            ) student_kim
+                                        );
+                                        
 -- 16. 성적 테이블에서 휴학생들의 성적 항목을 제거하시오.
+SELECT * FROM tb_student;
+SELECT * FROM tb_grade;
 
+DELETE 
+FROM tb_grade
+WHERE student_no IN (SELECT student_no
+                    FROM tb_student
+                    WHERE absence_yn = 'Y');
+
+SELECT
+    student_no
+    , absence_yn
+    , point
+FROM
+    tb_student
+    JOIN tb_grade USING(student_no)
+WHERE
+    absence_yn = 'Y';
 
 
 
 -- 17. 위의 변경 또는 삭제된 내용들을 확정짓기 위한 커밋을 진행하시오.
+COMMIT;
